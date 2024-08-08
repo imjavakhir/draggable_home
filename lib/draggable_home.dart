86d80a6c8e86c236fb33b691ed7f6e5def666dff
Javoxir Abdumalikov <javakhir_6272@mail.ru>
2024-08-08 09:52:20 +0500

    fixed

--- lib/draggable_home.dart
+++ lib/draggable_home.dart
@@ -246,7 +246,7 @@ class _DraggableHomeState extends State<DraggableHome> {
                           ),
                         ),
                         Positioned(
-                          bottom: 0,
+                          bottom: -2,
                           top: 0,
                           left: 0,
                           right: 0,
